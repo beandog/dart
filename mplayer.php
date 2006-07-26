d71a140f280106a7771c3985442b099ba86f634e
steve <steve@f4574214-f815-0410-b9ce-81fe6a62d61e>
2006-07-26 16:51:49 +0000

    updates
    git-svn-id: file:///var/svn/bend@65 f4574214-f815-0410-b9ce-81fe6a62d61e

--- mplayer.php
+++ mplayer.php
@@ -1,75 +1,72 @@
 #!/usr/bin/php
 <?
 
-	$save_files_to = "/home/steve/.mplayer/playback/";
+	$save_files_to = "/home/sdibb/.mplayer/playback/";
 
 	/**
 	 *
 	 * @author Steve Dibb <steve dot dibb at gmail dot com>
 	 * @copyright public-domain
-	 * @date 2006-07-16
-	 * @version 1.1
+	 * @date 2006-07-26
+	 * @version 1.2
 	 * @homepage http://wonkabar.org/bend/
 	 * 
-	 * Run time dependencies: PHP 5
+	 * Run time dependencies: PHP >= 4.3.0 with CLI
 	 *
-	 * MPLayer playback / resume position script
+	 * MPlayer playback / resume position script
 	 * 
 	 * 	This PHP script will save the playback position of a file
-	 * 	you use with MPLayer.  Once you start playing the same file
-	 * 	again, it will resume from last position.
+	 * 	you watch with MPlayer.  Once you start playing the same file
+	 * 	again, it will resume from last playback position.
 	 *
 	 * 	The *only* way this script will work is if you pass the
-	 * 	"get_time_pos" command to MPlayer through a keypress event,
+	 * 	"get_time_pos" command to MPlayer through an input event,
 	 *	mapped by either LIRC or the keyboard.
 	 *	
 	 *	The script captures the output and saves it to a file in
-	 *	for each media file.
+	 *	the playback directory for each media file.
 	 *
-	 *	You can also do it manually using mplayer's slave mode,
-	 *	which might be useful for debugging:
-	 *
-	 *		$ mplayer -slave -quiet foo-bar.avi
-	 *		get_time_pos
-	 *		quit
-	 *
-	 *	See http://www.mplayerhq.hu/DOCS/tech/slave.txt for commands
-	 *	that work on both backends.
+	 *	See http://www.mplayerhq.hu/DOCS/tech/slave.txt for more
+	 *	commands that work on both backends.
 	 *
 	 * LIRC:
 	 *
 	 * 	Setup your ~/.lircrc file to print out the playback position
-	 *  	and then quit playing the file.
+	 *  	_and_ then quit playing the file.
 	 * 
 	 *		begin
 	 *			prog = mplayer
-	 *			button = exit
+	 *			button = stop
 	 *			config = get_time_pos
 	 *		end
 	 *		begin
 	 *			prog = mplayer
-	 *			button = exit
+	 *			button = stop
 	 *			config = quit
 	 *		end
 	 *
 	 * Keyboard:
 	 *
-	 *	Just map a key with ~/.mplayer/input.conf to run 'get_time_pos'
+	 *	Map a key with ~/.mplayer/input.conf to run 'get_time_pos'
 	 *
 	 *	Sample entry:
 	 *
 	 *		g get_time_pos
 	 *
-	 *	Then when you want to save the position, hit 'g', and then 'q'
+	 *	When you want to save the position, hit 'g', and then 'q'
 	 *	to quit playback.
 	 *
-	 *	Note that if you use it this way, the script willy only save
-	 *	the playback position when you hit 'g', not when you quit.
+	 *	You can also do it manually using mplayer's slave mode,
+	 *	which is useful for debugging:
+	 *
+	 *		$ mplayer -slave -quiet foo-bar.avi
+	 *		get_time_pos
+	 *		quit
 	 *
 	 * Configuration:
 	 *
-	 *	This script will save a text file for each movie you
-	 * 	watch using this script.  The files are saved in the
+	 *	This script will save a text file for each media file you
+	 * 	watch using this script.  The text files are saved in the
 	 * 	config variable $save_files_to defined above, which
 	 *	must be a directory that this script can read / write to.
 	 *
@@ -82,25 +79,56 @@
 	 *
 	 *	$ php mplayer.php foo-bar.avi
 	 *
+	 *	# cp mplayer.php /usr/local/bin/mplayer-resume
+	 *	# chmod +x /usr/local/bin/mplayer-resume
+	 *	$ mplayer-resume foo-bar.avi
+	 *
 	 *	The script should parses any arguments that you want to send
-	 * 	mplayer at the same time.
+	 * 	mplayer at the same time, irregardless of whether you start
+	 *	it as a standalone program or with php.
 	 *
 	 *	$ php mplayer.php foo-bar.avi -vo xv
+	 *
+	 *	$ mplayer-resume foo-bar.avi -vo xv
 	 * 
 	 *	The *only* extra arguments that this script will add to your
 	 *	mplayer command is -ss to seek to resume a file, and
 	 *	-quiet so it can correctly grab the output.
 	 *
+	 * Notes:
+	 *
+	 *	If your CLI version of PHP is dumping version information
+	 *	each time it runs, add -q to the top line to supress the
+	 *	output.
+	 *
+	 *		#!/usr/bin/php -q
+	 *
+	 *	If you are starting the script from an external program,
+	 *	don't put quotes around the filenames.  It won't work on
+	 *	files with special characters.
+	 *
+	 *	Wrong:	$ mplayer-resume "$1"
+	 *	Right:	$ mplayer-resume $1
+	 *
 	 * Bugs:
 	 *
-	 *	Won't play files with spaces in them
+	 *	- Won't play files with spaces in them
+	 *	- Doesn't save cwd with the file, so you can only have one
+	 *	  entry per filename
 	 *
 	 */
 
+	// Check to see the directory exists.  If not, try and create it.
+	if(!is_dir($save_files_to)) {
+		if(!mkdir($save_files_to)) {
+			fwrite(STDOUT, "Please make sure the directory exists and is both readable and writable by this user\n");
+			die;
+		}
+	}
+
 	// Make sure I can save playback files
 	if(!is_writable($save_files_to) || !is_readable($save_files_to)) {
-		fwrite(STDOUT, "I can't read from or write to $save_files_to\n");
-		fwrite(STDOUT, "Please make sure the directory exists and is both readable and writable by this user\n");
+		fwrite(STDOUT, "I can't read from and/or write to $save_files_to\n");
 		die;
 	}
 
@@ -166,6 +194,17 @@
 	if($endpos < 1 && file_exists($txt))
 		unlink($txt);
 	// Save the (positive) playback position to the file
-	else
-		file_put_contents($txt, $endpos);
+	else {
+		// Generic error message, fix your stupid permissions
+		$error_msg = "Cannot save file, please check write permissions for this user in $save_files_to";
+
+		// Use PHP 5's fancy new functions, if possible. :)
+		if(function_exists('file_put_contents'))
+			file_put_contents($txt, $endpos) or die($error_msg);
+		// Otherwise fallback on PHP 4's functions
+		else {
+			#$handle = fopen($txt, 'w') or die($error_msg);
+			fwrite(fopen($txt, 'w'), $endpos) or die($error_msg);
+		}	
+	}
 ?>
