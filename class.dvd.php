f955a4efdf6b19763615ce8494f31f5b2f2b7e99
steve <steve@f4574214-f815-0410-b9ce-81fe6a62d61e>
2006-07-27 13:35:40 +0000

    tweak encoding settings
    git-svn-id: file:///var/svn/bend@76 f4574214-f815-0410-b9ce-81fe6a62d61e

--- class.dvd.php
+++ class.dvd.php
@@ -561,7 +561,7 @@
 				$flags = " -aid $aid ";
 			if($cartoon == 't') {
 				$xvidencopts = ':cartoon=1';
-				$vf = ",pullup,softskip";
+				#$vf = ",pullup,softskip";
 			}
 			
 			$basename = basename($vob, '.vob');
