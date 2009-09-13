9caa32f5097a584eb4faf39a1e65921efaa945c6
steve <steve@f4574214-f815-0410-b9ce-81fe6a62d61e>
2009-09-13 14:49:09 +0000

    dvd vob class
    git-svn-id: file:///var/svn/bend@228 f4574214-f815-0410-b9ce-81fe6a62d61e

---
+++ class.dvdvob.php
@@ -0,0 +1,87 @@
+<?
+
+	class DVDVOB {
+	
+		function __construct($vob = "movie.vob") {
+		
+			$this->setFilename($vob);
+			$this->aid = 128;
+			$this->debug = false;
+		
+		}
+		
+		function setFilename($vob) {
+			$this->filename = $vob;
+		}
+		
+		function getFilename() {
+			return $this->filename;
+		}
+		
+		function setAID($int = 128) {
+			$int = abs(intval($int));
+			$this->aid = $int;
+		}
+		
+		function getAID() {
+			return $this->aid;
+		}
+		
+		function setDebug($bool) {
+			$this->debug = (boolean)$bool;
+		}
+
+		/**
+		* Extract the raw video stream from a media file
+		*
+		* @param string destination filename
+		*/
+		function rawvideo($dest) {
+		
+			$flags = array('"'.$this->getFilename().'"', "-ovc copy", "-of rawvideo", "-nosound", "-quiet", "-o \"$dest\"");
+			
+			$str = "mencoder ".implode(' ', $flags);
+			
+			if($this->debug)
+				shell::msg("Executing: $str");
+			
+			$start = time();
+			shell::cmd($str, !$this->debug);
+			$finish = time();
+			
+			if($this->debug) {
+				$exec_time = shell::executionTime($start, $finish);
+				shell::msg("Execution time: ".$exec_time['minutes']."m ".$exec_time['seconds']."s");
+			}
+			
+		}
+		
+		/**
+		* Extract the raw audio stream from a media file
+		*
+		* @param string source filename
+		* @param string destination filename
+		*/
+		function rawaudio($dest) {
+		
+			$aid = abs(intval($aid));
+			
+			$flags = array('"'.$this->getFilename().'"', "-oac copy", "-of rawaudio", "-ovc frameno", "-quiet", "-aid ".$this->getAID(), "-o \"$dest\"");
+			
+			$str = "mencoder ".implode(' ', $flags);
+			
+			if($this->debug)
+				shell::msg("Executing: $str");
+			
+			$start = time();
+			shell::cmd($str, !$this->debug);
+			$finish = time();
+			
+			if($this->debug) {
+				$exec_time = shell::executionTime($start, $finish);
+				shell::msg("Execution time: ".$exec_time['minutes']."m ".$exec_time['seconds']."s");
+			}
+		
+		}
+	}
+?>
