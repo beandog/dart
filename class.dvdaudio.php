1bf711367b01e0916954c9dc1f364f5e83c17f45
Steve Dibb <beandog@gentoo.org>
2010-07-11 19:27:27 -0600

    New class to access DVD audio streams

---
+++ class.dvdaudio.php
@@ -0,0 +1,64 @@
+<?
+
+	class DVDAudio {
+	
+		// 2 character code for the language
+		private $langcode;
+		
+		// Full string for the language
+		private $language;	
+		
+		// Audio codec format (AC3, DTS)
+		private $format;
+		
+		// Number of audio channels
+		private $channels;
+		
+		// Stream ID for audio
+		private $stream_id;
+		
+		function __construct($xml, $stream_id = "0x80") {
+			$this->parse_xml($xml, $stream_id);
+		}
+		
+		/** Metadata **/
+		private function parse_xml($str, $stream_id) {
+		
+			$sxe = simplexml_load_string($str) or die("Couldn't parse lsdvd XML output");
+			
+			// Audio Tracks
+			foreach($sxe->track->audio as $audio) {
+				
+				$xml_stream_id = (string)$audio->streamid;
+				
+				if($xml_stream_id == $stream_id) {
+					$this->langcode = (string)$audio->langcode;
+					$this->language = (string)$audio->language;
+					$this->format = (string)$audio->format;
+					$this->channels = (int)$audio->channels;
+					$this->stream_id = (string)$audio->streamid;
+				}
+				
+			}
+		
+		}
+		
+		public function getLanguage() {
+			return $this->language;
+		}
+		
+		public function getLangcode() {
+			return $this->langcode;
+		}
+		
+		function getChannels() {
+			return $this->channels;
+		}
+		
+		function getFormat() {
+			return $this->format;
+		}
+		
+	}
+	
+?>
