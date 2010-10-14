a6c8b09a7383a77707ac1c19e48d5d1c61952ad7
Steve Dibb <beandog@gentoo.org>
2010-10-13 21:47:20 -0600

    Don't display errors if making directories

--- drip.php
+++ drip.php
@@ -783,7 +783,7 @@
 		
 		// Create export dir
 		if(!is_dir($drip->export))
-			mkdir($drip->export, 0755);
+			@mkdir($drip->export, 0755);
 			
 		// Pull out the tracks that haven't been flagged to ignore in the database frontend
 		// This query has nothing to do with what has / hasn't been encoded
