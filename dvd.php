0c4d4a39dd4a0fbbd6df37d0c0fa8444df6970e2
Steve Dibb <beandog@gentoo.org>
2011-02-27 16:44:45 -0700

    updaets

--- dvd.php
+++ dvd.php
@@ -9,59 +9,78 @@
 	require_once "class.dvdaudio.php";
 	require_once "class.dvdsubs.php";
 	
-	$dvd = new DVD();
-	
 	$action = $argv[1];
+	$device = $argv[2];
+
+	if(empty($device))
+		$device = "/dev/dvd";
+
+	$dvd = new DVD($device);
 	
 	if($action == "import") {
 	
 		$uniq_id = $dvd->getID();
 	
 		$d = dvds::find_by_uniq_id($uniq_id);
-		
+
 		if(is_null($d->id)) {
 		
 			$arr = array(
 				'uniq_id' => $uniq_id,
-				'title' => $dvd->getTitle(),
-				'vmg_id' => $dvd->getVMGID(),
-				'provider_id' => $dvd->getProviderID(),
-				'longest_track' => $dvd->getLongestTrack(),
 			);
 		
 			$d = dvds::create($arr);
 		
 		}
 		
+		$arr = array(
+			'title' => $dvd->getTitle(),
+			'vmg_id' => $dvd->getVMGID(),
+			'provider_id' => $dvd->getProviderID(),
+			'longest_track' => $dvd->getLongestTrack(),
+		);
+		
+		$d->update_attributes($arr);
+			
+		$d->save();
+		
+		/** Tracks **/
+		
 		$num_tracks = $dvd->getNumTracks();
 		
 		for($track_number = 1; $track_number <= $num_tracks; $track_number++) {
 		
-			$dvd_track = new DVDTrack($track_number);
+			$dvd_track = new DVDTrack($track_number, $device);
 			
 			$track = tracks::first(array('conditions' => array('dvd_id' => $d->id, 'ix' => $track_number)));
-			
+
 			if(is_null($track->id)) {
 			
 				$arr = array(
 					'dvd_id' => $d->id,
 					'ix' => $dvd_track->getIX(),
-					'length' => $dvd_track->getLength(),
-					'vts_id' => $dvd_track->getVTSID(),
-					'vts' => $dvd_track->getVTS(),
-					'ttn' => $dvd_track->getTTN(),
-					'fps' => $dvd_track->getFPS(),
-					'format' => $dvd_track->getVideoFormat(),
-					'aspect' => $dvd_track->getAspectRatio(),
-					'width' => $dvd_track->getWidth(),
-					'height' => $dvd_track->getHeight(),
-					'df' => $dvd_track->getDF(),
-					'angles' => $dvd_track->getAngles(),
 				);
 				
 				$track = tracks::create($arr);
 				
 			}
+			
+			$arr = array(
+				'length' => $dvd_track->getLength(),
+				'vts_id' => $dvd_track->getVTSID(),
+				'vts' => $dvd_track->getVTS(),
+				'ttn' => $dvd_track->getTTN(),
+				'fps' => $dvd_track->getFPS(),
+				'format' => $dvd_track->getVideoFormat(),
+				'aspect' => $dvd_track->getAspectRatio(),
+				'width' => $dvd_track->getWidth(),
+				'height' => $dvd_track->getHeight(),
+				'df' => $dvd_track->getDF(),
+				'angles' => $dvd_track->getAngles(),
+			);
+			
+			$track->update_attributes($arr);
+			$track->save();
 			
 			// Get lsdvd XML to pass to sub-classes
 			$xml = $dvd_track->getXML();
@@ -99,23 +118,23 @@
 				
 				$audio = audio::first(array('conditions' => $arr));
 				
-				if(is_null($audio->id)) {
-				
-					$arr = array_merge($arr, array(
-						'langcode' => $dvd_audio->getLangcode(),
-						'language' => $dvd_audio->getLanguage(),
-						'format' => $dvd_audio->getFormat(),
-						'frequency' => $dvd_audio->getFrequency(),
-						'quantization' => $dvd_audio->getQuantization(),
-						'channels' => $dvd_audio->getChannels(),
-						'ap_mode' => $dvd_audio->getAPMode(),
-						'content' => $dvd_audio->getContent(),
-						'streamid' => $streamid,
-					));
-					
+				if(is_null($audio->id)) 
 					$audio = audio::create($arr);
-					
-				}
+				
+				$arr = array(
+					'langcode' => $dvd_audio->getLangcode(),
+					'language' => $dvd_audio->getLanguage(),
+					'format' => $dvd_audio->getFormat(),
+					'frequency' => $dvd_audio->getFrequency(),
+					'quantization' => $dvd_audio->getQuantization(),
+					'channels' => $dvd_audio->getChannels(),
+					'ap_mode' => $dvd_audio->getAPMode(),
+					'content' => $dvd_audio->getContent(),
+					'streamid' => $streamid,
+				);
+				
+				$audio->set_attributes($arr);
+				$audio->save();
 				
 			}
 			
@@ -135,18 +154,18 @@
 				
 				$subp = subp::first(array('conditions' => $arr));
 				
-				if(is_null($subp->id)) {
-				
-					$arr = array_merge($arr, array(
-						'langcode' => $dvd_subp->getLangcode(),
-						'language' => $dvd_subp->getLanguage(),
-						'content' => $dvd_subp->getContent(),
-						'streamid' => $streamid,
-					));
-					
+				if(is_null($subp->id))
 					$subp = subp::create($arr);
-					
-				}
+				
+				$arr = array(
+					'langcode' => $dvd_subp->getLangcode(),
+					'language' => $dvd_subp->getLanguage(),
+					'content' => $dvd_subp->getContent(),
+					'streamid' => $streamid,
+				);
+				
+				$subp->set_attributes($arr);
+				$subp->save();
 				
 			}
 			
@@ -154,16 +173,20 @@
 			
 			$dvd_chapters = $dvd_track->getChapters();
 			
-			foreach($dvd_chapters as $chapter_number => $arr) {
+			foreach($dvd_chapters as $chapter_number => $chapter_data) {
 			
-				$arr = array_merge($arr, array(
+				$arr = array(
 					'track_id' => $track->id,
-				));
+					'ix' => $chapter_data['ix'],
+				);
 				
 				$chapters = chapters::first(array('conditions' => $arr));
 				
  				if(is_null($chapters->id))
  					$chapters = chapters::create($arr);
+ 					
+ 				$chapters->set_attributes($chapter_data);
+ 				$chapters->save();
 				
 			}
 			
