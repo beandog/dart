138f76b658cdb866e665c8de0d700df5ccb4a861
Steve Dibb <beandog@gentoo.org>
2011-04-15 21:10:49 -0600

    Remove inc.dart.php; Disable --info

--- dart.php
+++ dart.php
@@ -3,8 +3,28 @@
 
 	require_once 'Console/ProgressBar.php';
 
-	// dart Functions
-	require_once 'inc.dart.php';
+	require_once 'class.shell.php';
+	require_once 'class.dart.php';
+
+	require_once 'ar/pg.dvds.php';
+	
+	require_once 'class.dvd.php';
+	require_once 'class.dvdvob.php';
+	require_once 'class.dvdtrack.php';
+	require_once 'class.dvdaudio.php';
+	require_once 'class.dvdsubs.php';
+	require_once 'class.matroska.php';
+	require_once 'class.handbrake.php';
+	
+	require_once 'models/dvds.php';
+	require_once 'models/episodes.php';
+	require_once 'models/series_dvds.php';
+	require_once 'models/series.php';
+	require_once 'models/tracks.php';
+	require_once 'models/queue.php';
+	
+	$dart = new dart();
+	
 	$queue_model = new Queue_Model;
 	$dvd_episodes = array();
 	
@@ -181,12 +201,13 @@
 		
 	}
 	
+	// FIXME
 	// Display info about disc
-	if($info)
-		if($disc_archived)
-			display_info($uniq_id);
-		else
-			shell::msg("Disc is not archived");
+// 	if($info)
+// 		if($disc_archived)
+// 			display_info($uniq_id);
+// 		else
+// 			shell::msg("Disc is not archived");
 	
 	/**
 	 * --iso
@@ -656,6 +677,44 @@
 
 		}
 
+	}
+	
+	function display_help() {
+	
+		shell::msg("Options:");
+		shell::msg("  --info\t\tList episodes on DVD");
+		
+		shell::msg("  --rip\t\t\tRip everything on DVD");
+		shell::msg("  --nosub\t\tDon't rip VobSub subtitles");
+		shell::msg("  --encode\t\tEncode episodes in queue");
+		
+		shell::msg("  --archive\t\tAdd DVD to database");
+		shell::msg("  --season <int>\tSet season #");
+		shell::msg("  --volume <int>\tSet volume #");
+		shell::msg("  --disc <int>\t\tSet disc # for season");
+		shell::msg("  --series <int>\tPass TV Series ID");
+		
+		shell::msg("  --demux\t\tUse MEncoder to demux audio and video streams into separate files");
+		
+		shell::msg("  --skip <int>\t\tSkip # of episodes");
+		shell::msg("  --max <int>\t\tMax # of episodes to rip and/or encode");
+		shell::msg("  -v, -verbose\t\tVerbose output");
+		shell::msg("  --debug\t\tEnable debugging");
+		shell::msg("  --update\t\tUpdate DVD specs in database");
+		shell::msg("  -q, --queue\t\tList episodes in queue");
+		
+		shell::msg("Subtitles:");
+		shell::msg("  --vobsub\t\tRip and mux VobSub subtitles");
+		shell::msg("  --cc\t\t\tRip and mux Closed Captioning subtitles");
+		
+		shell::msg("Handbrake:");
+		shell::msg("  --handbrake\t\tUse Handbrake to reencode video");
+		shell::msg("  --preset\t\tEncoding preset to use");
+		
+		shell::msg("Movies:");
+		shell::msg("  --movie\t\tUse some settings to archive as a movie");
+		shell::msg("  --title\t\tMovie Title");
+	
 	}
 	
 		
