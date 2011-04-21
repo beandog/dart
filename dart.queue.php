a61006c728a3fe4df6541398f6d774a7ab0bfd08
Steve Dibb <beandog@gentoo.org>
2011-04-20 21:41:15 -0600

    Display episode # on verbose

--- dart.queue.php
+++ dart.queue.php
@@ -12,8 +12,18 @@
 	
 		foreach($queue_episodes as $episode_id) {
 			$episodes_model = new Episodes_Model($episode_id);
-			$display_name = $episodes_model->get_display_name();
-			echo("$display_name\n");
+			$str = $episodes_model->get_display_name();
+			
+			if($verbose) {
+				
+				$episode_number = $episodes_model->get_number();
+				
+				if($episode_number)
+					$str .= " (#$episode_number)";
+				
+			}
+			
+			echo("$str\n");
 		}
 	
 	}
