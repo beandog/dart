ab6b44d8e695100bd0526e4c6023a766bb21894b
Steve Dibb <beandog@gentoo.org>
2011-04-23 22:54:28 -0600

    Specify only to send MKV files over FTP

--- dart.ftp.php
+++ dart.ftp.php
@@ -3,7 +3,7 @@
 	if($ftp) {
 	
 		chdir("/home/steve/dvds");
-		$exec = "find . -mindepth 1 -type d -print0 | xargs -0 -I {} ncftpput -m -z -R -DD zotac /var/media/updates/ {}";
+		$exec = "find . -mindepth 1 -type f -name '*.mkv' -print0 | xargs -0 -I {} ncftpput -m -z -R -DD zotac /var/media/updates/ {}";
 		passthru($exec);
 	
 	}
