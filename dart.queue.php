f4eeb1d469ad92336e25fb00f5669bcb74e440c9
Steve Dibb <beandog@gentoo.org>
2011-04-24 14:15:53 -0600

    Tweak verbosity

--- dart.queue.php
+++ dart.queue.php
@@ -10,16 +10,16 @@
 	
 	if($queue) {
 	
+		if(count($queue_episodes))
+			echo "[Total: ".count($queue_episodes)."]\n";
+	
 		foreach($queue_episodes as $episode_id) {
 			
 			$episodes_model = new Episodes_Model($episode_id);
 			$str = $episodes_model->get_display_name();
 			
-			if($verbose) {
-				
+			if($verbose)
 				$str = get_episode_filename($episode_id);
-				
-			}
 			
 			if($verbose)
 				$str .= " (".$episodes_model->get_iso().")";
