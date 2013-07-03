5a7431c6b5343ec9e9451175e83795c0e2e69625
Steve Dibb <steve.dibb@gmail.com>
2013-07-02 20:41:57 -0600

    Move pg connnection into includes

---
+++ includes/pgconn.php
@@ -0,0 +1,13 @@
+<?php
+	require_once("php-activerecord/ActiveRecord.php");
+
+	$connections = array(
+    		'pgsql' => 'pgsql://steve@localhost/dvds',
+	);
+
+	ActiveRecord\Config::initialize(function($cfg) use ($connections) {
+		$cfg->set_model_directory('/home/steve/php/models');
+		$cfg->set_connections($connections);
+ 
+		$cfg->set_default_connection('pgsql');
+	});
