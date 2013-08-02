323fd5826c98593998625eff643bc240de701de3
Steve Dibb <steve.dibb@gmail.com>
2013-08-02 12:22:30 -0600

    New model for subtitles

---
+++ models/subp.php
@@ -0,0 +1,34 @@
+<?
+
+	class Subp_Model extends DBTable {
+
+		function __construct($id = null) {
+
+			$table = "subp";
+
+			$this->id = parent::__construct($table, $id);
+
+		}
+
+		/**
+		 * Do a lookup for a primary key based on
+		 * track_id and ix.
+		 *
+		 * Note that normally it seems like looking up on
+		 * streamid would make more sense, but the original
+		 * activerecord code would do a lookup on 'ix' instead,
+		 * so I'm keeping it the same.
+		 *
+		 * @params int $track_id subp.track_id
+		 * @params int $ix subp.ix
+		 */
+		public function find_subp_id($track_id, $ix) {
+
+			$sql = "SELECT id FROM subp WHERE track_id = ".$this->db->quote($track_id)." AND ix = ".$this->db->quote($ix).";";
+			$var = $this->db->getOne($sql);
+
+			return $var;
+		}
+
+	}
+?>
