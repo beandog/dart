43f9d0551abdef2942c9078a736cc8c0b0602243
Steve Dibb <steve.dibb@gmail.com>
2013-08-02 12:05:46 -0600

    Switch lookup from on streamid to ix, based on the original code

--- models/audio.php
+++ models/audio.php
@@ -12,14 +12,19 @@
 
 		/**
 		 * Do a lookup for a primary key based on
-		 * track_id and streamid.
+		 * track_id and ix.
+		 *
+		 * Note that normally it seems like looking up on
+		 * streamid would make more sense, but the original
+		 * activerecord code would do a lookup on 'ix' instead,
+		 * so I'm keeping it the same.
 		 *
 		 * @params int $track_id audio.track_id
-		 * @params int $streamid audio.streamid
+		 * @params int $ix audio.ix
 		 */
-		public function find_audio_id($track_id, $streamid) {
+		public function find_audio_id($track_id, $ix) {
 
-			$sql = "SELECT id FROM audio WHERE track_id = ".$this->db->quote($track_id)." AND streamid = ".$this->db->quote($streamid).";";
+			$sql = "SELECT id FROM audio WHERE track_id = ".$this->db->quote($track_id)." AND ix = ".$this->db->quote($ix).";";
 			$var = $this->db->getOne($sql);
 
 			return $var;
