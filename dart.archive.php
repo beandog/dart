9a0deafedbaf186cc175bb1e24fefe51d559ac9d
Steve Dibb <steve.dibb@gmail.com>
2013-08-02 15:12:19 -0600

    Run individual checks for missing metadata on each section

--- dart.archive.php
+++ dart.archive.php
@@ -9,34 +9,42 @@
 	 * compile in here, and be stored centrally.
 	 */
 
-	if($access_device && $disc_indexed && !$disc_archived) {
+	if($access_device && $disc_indexed) {
 
 		if($verbose)
 			shell::stdout("[Metadata]");
 
+		// Use database checks to see if archiving needs to happen
+		$missing_dvd_metadata = $dvds_model->missing_metadata();
+
 		/** DVDS **/
 
-		// DVD longest track
-		if(is_null($dvds_model->longest_track)) {
-			if($verbose)
-				shell::stdout("* Updating longest track");
-			$dvds_model->longest_track = $dvd->getLongestTrack();
-		}
+		if($missing_dvd_metadata) {
 
-		// DVD filesize
-		if(is_null($dvds_model->filesize)) {
-			if($verbose)
-				shell::stdout("* Updating filesize");
-			$dvds_model->filesize = $dvd->getSize();
-		}
+			// DVD longest track
+			if(is_null($dvds_model->longest_track)) {
+				if($verbose)
+					shell::stdout("* Updating longest track");
+				$dvds_model->longest_track = $dvd->getLongestTrack();
+			}
 
-		// DVD serial ID
-		// Not using 'empty' because it's a constructor, and
-		// not a function.
-		if(!($dvds_model->serial_id)) {
-			if($verbose)
-				shell::stdout("* Updating serial ID");
-			$dvds_model->serial_id = $dvd->getSerialID();
+			// DVD filesize
+			if(is_null($dvds_model->filesize)) {
+				if($verbose)
+					shell::stdout("* Updating filesize");
+				$dvds_model->filesize = $dvd->getSize();
+			}
+
+			// DVD serial ID
+			// Not using 'empty' because it's a constructor, and
+			// not a function.
+			if(!($dvds_model->serial_id)) {
+				if($verbose)
+					shell::stdout("* Updating serial ID");
+				$dvds_model->serial_id = $dvd->getSerialID();
+			}
+
 		}
 
 	}
+?>
