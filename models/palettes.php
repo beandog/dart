5345b620a44df59cb6e4304710027a8560e0ddaf
Steve Dibb <steve.dibb@gmail.com>
2013-08-02 11:49:09 -0600

    Add a lookup on the numbered index

--- models/palettes.php
+++ models/palettes.php
@@ -12,14 +12,15 @@
 
 		/**
 		 * Do a lookup for a primary key based on
-		 * track_id and color.
+		 * track_id, internal index and color string.
 		 *
 		 * @params int $track_id palettes.track_id
+		 * @params int $ix palettes.ix
 		 * @params int $color palettes.color
 		 */
-		public function find_palettes_id($track_id, $color) {
+		public function find_palettes_id($track_id, $ix, $color) {
 
-			$sql = "SELECT id FROM palettes WHERE track_id = ".$this->db->quote($track_id)." AND color = ".$this->db->quote($color).";";
+			$sql = "SELECT id FROM palettes WHERE track_id = ".$this->db->quote($track_id)." AND ix = ".$this->db->quote($ix)." AND color = ".$this->db->quote($color).";";
 			$var = $this->db->getOne($sql);
 
 			return $var;
