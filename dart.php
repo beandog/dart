e4ac15f0521542f5e2dd34fb3ffc42b302659c51
Steve Dibb <steve.dibb@gmail.com>
2013-08-02 11:23:01 -0600

    Include palettes model

--- dart.php
+++ dart.php
@@ -23,6 +23,7 @@
 	require_once 'models/series_dvds.php';
 	require_once 'models/series.php';
 	require_once 'models/tracks.php';
+	require_once 'models/palettes.php';
 	require_once 'models/queue.php';
 
 	require_once 'dart.parser.php';
