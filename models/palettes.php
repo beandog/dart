a644ec13892d5707abfee04469044664c1b69367
Steve Dibb <steve.dibb@gmail.com>
2013-08-02 11:24:50 -0600

    Fix variable name

--- models/palettes.php
+++ models/palettes.php
@@ -17,7 +17,7 @@
 		 * @params int $track_id palettes.track_id
 		 * @params int $color palettes.color
 		 */
-		public function find_palettes_id($track_id, $ix) {
+		public function find_palettes_id($track_id, $color) {
 
 			$sql = "SELECT id FROM palettes WHERE track_id = ".$this->db->quote($track_id)." AND color = ".$this->db->quote($color).";";
 			$var = $this->db->getOne($sql);
