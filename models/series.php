2e0d88d0bff17754c1ff0e59df9503f41f52745c
Steve Dibb <steve.dibb@gmail.com>
2014-07-20 15:13:30 -0600

    Drop base preset

--- models/series.php
+++ models/series.php
@@ -96,16 +96,6 @@
 
 		}
 
-		function get_handbrake_base_preset() {
-
-			$sql = "SELECT presets.base_preset FROM presets INNER JOIN series_presets ON series_presets.preset_id = presets.id AND series_presets.series_id = ".$this->db->quote($this->id).";";
-
-			$var = $this->db->getOne($sql);
-
-			return $var;
-
-		}
-
 		function get_x264opts() {
 
 			$arr = array();
