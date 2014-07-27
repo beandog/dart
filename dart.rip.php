b6f52d13a088d42602bb57734c8818418da139ac
Steve Dibb <steve.dibb@gmail.com>
2014-07-27 17:53:34 -0600

    Create the queue directory if the target Matroska file doesn't exist

--- dart.rip.php
+++ dart.rip.php
@@ -55,24 +55,26 @@
 
 				$series_queue_dir = $export_dir."queue/".formatTitle($series_title);
 
-				/** Create directory to dump files to */
-				if(!is_dir($series_queue_dir))
-					mkdir($series_queue_dir, 0755, true);
-
-
-				// Create a symlink to the ISO in the queue directory
-				$series_queue_dir_iso = $series_queue_dir."/".$episodes_model->get_iso();
-				if(!file_exists($series_queue_dir_iso))
-					symlink($device_realpath, $series_queue_dir_iso);
-
 				$episode_filename = $export_dir."episodes/".$episode_filename;
 				$mkv = "$episode_filename.mkv";
 
 				// Check to see if file exists, if not, rip it
 				if(!$max || ($max && $num_queued < $max)) {
+
 					if(!file_exists($mkv)) {
+
 						$queue_model->add_episode($episode_id, php_uname('n'));
 						$num_queued++;
+
+						/** Create directory to dump files to */
+						if(!is_dir($series_queue_dir))
+							mkdir($series_queue_dir, 0755, true);
+
+						// Create a symlink to the ISO in the queue directory
+						$series_queue_dir_iso = $series_queue_dir."/".$episodes_model->get_iso();
+						if(!file_exists($series_queue_dir_iso))
+							symlink($device_realpath, $series_queue_dir_iso);
+
 					}
 
 					// Bump up the queue if we are accessing the drive directly
