ca1f1d8b435dc8005ed53684eb4abf9e19df9986
Steve Dibb <steve.dibb@gmail.com>
2014-07-27 20:51:27 -0600

    Clean up display output

--- dart.queue.php
+++ dart.queue.php
@@ -12,25 +12,52 @@
 
 	if($queue) {
 
-		if(count($queue_episodes))
-			echo "[Total: ".count($queue_episodes)."]\n";
-		else
-			echo "* Queue is empty\n";
+		echo "[DVD Queue: ".count($queue_episodes)." episodes]\n";
+
+		$counter = 1;
+
+		$display_iso = "";
 
 		foreach($queue_episodes as $episode_id) {
 
 			$episodes_model = new Episodes_Model($episode_id);
-			$str = $episodes_model->get_display_name();
+			$series_id = $episodes_model->get_series_id();
+			$series_model = new Series_Model($series_id);
+			$series_title = $series_model->title;
 
-			if($verbose) {
-				$str = get_episode_filename($episode_id);
-				$str = $export_dir.$str;
+			$series = array(
+				'id' => $series_id,
+				'title' => $series_title,
+				'queue_dir' => $export_dir."queue/".formatTitle($series_title),
+				'volume' => $episodes_model->get_volume(),
+			);
+
+			$episode = array(
+				'id' => $episode_id,
+				'title' => $episodes_model->title,
+				'season' => $episodes_model->get_season(),
+				'part' => $episodes_model->part,
+				'number' => $episodes_model->get_number(),
+				'ix' => $episodes_model->ix,
+				'starting_chapter' => $episodes_model->starting_chapter,
+				'ending_chapter' => $episodes_model->ending_chapter,
+				'filename' => basename(get_episode_filename($episode_id)),
+				'src_iso' => $series['queue_dir']."/".$episodes_model->get_iso(),
+				'queue_dir' => $export_dir."queue/".formatTitle($series_title)."/$episode_id.".formatTitle($episodes_model->title),
+				'dest_dir' => $export_dir."episodes/".formatTitle($series_title),
+				'dest_mkv' => $export_dir."episodes/".formatTitle($series_title)."/".basename(get_episode_filename($episode_id)).".mkv",
+			);
+
+			$episode_iso = $episodes_model->get_iso();
+
+			if($episode_iso != $display_iso) {
+				echo "* $episode_iso\n";
+				$display_iso = $episode_iso;
 			}
 
-			if($verbose)
-				$str .= " (".$episodes_model->get_iso().")";
+			echo "- $series_title: ".$episode['title']."\n";
 
-			echo "$str\n";
+			$counter++;
 		}
 
 	}
