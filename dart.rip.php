6dea68ed174f8250fb9c873955045ff7ee8ceec7
Steve Dibb <steve.dibb@gmail.com>
2014-07-27 20:51:45 -0600

    Cleanup display

--- dart.rip.php
+++ dart.rip.php
@@ -28,47 +28,55 @@
 			echo "Check the frontend to see if titles need to be added.\n";
 		} else {
 
-			$bar = new Console_ProgressBar('[%bar%] %percent%'." ($num_episodes episodes)", ':', ' :D ', 80, $num_episodes);
+			if($max) {
+				$max_episodes = min($max, $num_episodes);
+				echo "* $max_episodes episodes max.\n";
+			} else
+				$max_episodes = $num_episodes;
+
+			$bar = new Console_ProgressBar("[%bar%] %percent%", ":", " :D ", 80, $max_episodes);
 			$i = 0;
 
 			foreach($dvd_episodes as $episode_id) {
 
-				clearstatcache();
-
 				$episodes_model = new Episodes_Model($episode_id);
-				$series_id = $episodes_model->get_series_id();
-				$series_model = new Series_Model($series_id);
-				$series_title = $series_model->title;
-				$tracks_model = new Tracks_Model($episodes_model->track_id);
- 				$track_number = $tracks_model->ix;
+				$queue_episode_status = $queue_model->get_episode_status($episode_id);
 
-				$episode = array();
-				$series = array();
-				$collection = array();
+				if((!$max || ($max && $num_queued < $max)) && is_null($queue_episode_status)) {
 
-				$series = array(
-					'id' => $series_id,
-					'title' => $series_title,
-					'queue_dir' => $export_dir."queue/".formatTitle($series_title),
-				);
+					clearstatcache();
 
-				$episode = array(
-					'id' => $episode_id,
-					'title' => $episodes_model->title,
-					'season' => $episodes_model->get_season(),
-					'part' => $episodes_model->part,
-					'filename' => basename(get_episode_filename($episode_id)),
-					'src_iso' => $series['queue_dir']."/".$episodes_model->get_iso(),
-					'dest_dir' => $export_dir."episodes/".formatTitle($series_title),
-					'dest_mkv' => $export_dir."episodes/".formatTitle($series_title)."/".basename(get_episode_filename($episode_id)).".mkv",
-				);
+					$series_id = $episodes_model->get_series_id();
+					$series_model = new Series_Model($series_id);
+					$series_title = $series_model->title;
+					$tracks_model = new Tracks_Model($episodes_model->track_id);
+					$track_number = $tracks_model->ix;
 
-				$collection = array(
-					'title' => $series_model->get_collection_title(),
-				);
+					$episode = array();
+					$series = array();
+					$collection = array();
 
-				// Check to see if file exists, if not, rip it
-				if(!$max || ($max && $num_queued < $max)) {
+					$series = array(
+						'id' => $series_id,
+						'title' => $series_title,
+						'queue_dir' => $export_dir."queue/".formatTitle($series_title),
+					);
+
+					$episode = array(
+						'id' => $episode_id,
+						'title' => $episodes_model->title,
+						'season' => $episodes_model->get_season(),
+						'part' => $episodes_model->part,
+						'filename' => basename(get_episode_filename($episode_id)),
+						'src_iso' => $series['queue_dir']."/".$episodes_model->get_iso(),
+						'dest_dir' => $export_dir."episodes/".formatTitle($series_title),
+						'dest_mkv' => $export_dir."episodes/".formatTitle($series_title)."/".basename(get_episode_filename($episode_id)).".mkv",
+					);
+
+					$collection = array(
+						'title' => $series_model->get_collection_title(),
+					);
+
 
 					if(!file_exists($episode['dest_mkv'])) {
 
@@ -83,22 +91,23 @@
 						if(!file_exists($episode['src_iso']))
 							symlink($device_realpath, $episode['src_iso']);
 
+						$i++;
+
+						$bar->update($i);
+
 					}
 
 					// Bump up the queue if we are accessing the drive directly
 					if($device_is_hardware)
 						$queue_model->prioritize();
 
+
 				}
 
-				$i++;
-
-				$bar->update($i);
-
-				if(($i + 1) == $max)
-					break(2);
-
 			}
+
+			echo "\n[Queue]\n";
+			echo "* Total episodes in queue: ".count($queue_model->get_episodes($hostname));
 
 		}
 
