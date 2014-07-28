b22ba8b8592670f7846a758960c2d6f5c86158a3
Steve Dibb <steve.dibb@gmail.com>
2014-07-27 18:24:58 -0600

    Clean up naming scheme; create queue directory when ripping; symlink source

--- dart.rip.php
+++ dart.rip.php
@@ -35,51 +35,60 @@
 
 				clearstatcache();
 
-				// New instance of a DB episode
 				$episodes_model = new Episodes_Model($episode_id);
-				$episode_season = $episodes_model->get_season();
-				$episode_title = $episodes_model->title;
-				$episode_part = $episodes_model->part;
-				$episode_filename = get_episode_filename($episode_id);
-
+				$series_id = $episodes_model->get_series_id();
+				$series_model = new Series_Model($series_id);
+				$series_title = $series_model->title;
 				$tracks_model = new Tracks_Model($episodes_model->track_id);
  				$track_number = $tracks_model->ix;
 
-				// Get the series ID
-				$series_id = $episodes_model->get_series_id();
+				$episode = array();
+				$series = array();
+				$collection = array();
 
-				// New instance of a DB series
-				$series_model = new Series_Model($series_id);
-				$collection_title = $series_model->get_collection_title();
-				$series_title = $series_model->title;
+				$series = array(
+					'id' => $series_id,
+					'title' => $series_title,
+					'queue_dir' => $export_dir."queue/".formatTitle($series_title),
+				);
 
-				$series_queue_dir = $export_dir."queue/".formatTitle($series_title);
+				$episode = array(
+					'id' => $episode_id,
+					'title' => $episodes_model->title,
+					'season' => $episodes_model->get_season(),
+					'part' => $episodes_model->part,
+					'filename' => basename(get_episode_filename($episode_id)),
+					'src_iso' => $series['queue_dir']."/".$episodes_model->get_iso(),
+					'dest_dir' => $export_dir."episodes/".formatTitle($series_title),
+					'dest_mkv' => $export_dir."episodes/".formatTitle($series_title)."/".basename(get_episode_filename($episode_id)).".mkv",
+				);
 
-				$episode_filename = $export_dir."episodes/".$episode_filename;
-				$mkv = "$episode_filename.mkv";
+				$collection = array(
+					'title' => $series_model->get_collection_title(),
+				);
 
 				// Check to see if file exists, if not, rip it
 				if(!$max || ($max && $num_queued < $max)) {
 
-					if(!file_exists($mkv)) {
+					if(!file_exists($episode['dest_mkv'])) {
 
 						$queue_model->add_episode($episode_id, php_uname('n'));
 						$num_queued++;
 
 						/** Create directory to dump files to */
-						if(!is_dir($series_queue_dir))
-							mkdir($series_queue_dir, 0755, true);
+						if(!is_dir($series['queue_dir']))
+							mkdir($series['queue_dir'], 0755, true);
 
 						// Create a symlink to the ISO in the queue directory
-						$series_queue_dir_iso = $series_queue_dir."/".$episodes_model->get_iso();
-						if(!file_exists($series_queue_dir_iso))
-							symlink($device_realpath, $series_queue_dir_iso);
+						if(!file_exists($episode['src_iso']))
+							symlink($device_realpath, $episode['src_iso']);
 
 					}
 
 					// Bump up the queue if we are accessing the drive directly
 					if($device_is_hardware)
 						$queue_model->prioritize();
+
 				}
 
 				$i++;
