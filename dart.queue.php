daeb04954bf4a1d746ca96cc72ab5d10d3bd53f4
Steve Dibb <steve.dibb@gmail.com>
2014-07-27 21:21:26 -0600

    Cleanup queue output

--- dart.queue.php
+++ dart.queue.php
@@ -12,11 +12,11 @@
 
 	if($queue) {
 
-		echo "[DVD Queue: ".count($queue_episodes)." episodes]\n";
-
 		$counter = 1;
 
 		$display_iso = "";
+
+		$d_series = array();
 
 		foreach($queue_episodes as $episode_id) {
 
@@ -25,39 +25,37 @@
 			$series_model = new Series_Model($series_id);
 			$series_title = $series_model->title;
 
-			$series = array(
-				'id' => $series_id,
-				'title' => $series_title,
-				'queue_dir' => $export_dir."queue/".formatTitle($series_title),
-				'volume' => $episodes_model->get_volume(),
-			);
-
-			$episode = array(
-				'id' => $episode_id,
-				'title' => $episodes_model->title,
-				'season' => $episodes_model->get_season(),
-				'part' => $episodes_model->part,
-				'number' => $episodes_model->get_number(),
-				'ix' => $episodes_model->ix,
-				'starting_chapter' => $episodes_model->starting_chapter,
-				'ending_chapter' => $episodes_model->ending_chapter,
-				'filename' => basename(get_episode_filename($episode_id)),
-				'src_iso' => $series['queue_dir']."/".$episodes_model->get_iso(),
-				'queue_dir' => $export_dir."queue/".formatTitle($series_title)."/$episode_id.".formatTitle($episodes_model->title),
-				'dest_dir' => $export_dir."episodes/".formatTitle($series_title),
-				'dest_mkv' => $export_dir."episodes/".formatTitle($series_title)."/".basename(get_episode_filename($episode_id)).".mkv",
-			);
-
 			$episode_iso = $episodes_model->get_iso();
 
 			if($episode_iso != $display_iso) {
-				echo "* $episode_iso\n";
 				$display_iso = $episode_iso;
 			}
 
-			echo "- $series_title: ".$episode['title']."\n";
+			$d_series[$series_title][$episode_iso][] = $episodes_model->title;
 
 			$counter++;
 		}
+
+		if($queue_episodes) {
+
+			foreach($d_series as $series_title => $arr_isos) {
+
+				echo "[$series_title]\n";
+
+				foreach($arr_isos as $iso => $arr_episodes) {
+
+					echo "* $iso - ".count($arr_episodes)." episodes\n";
+					echo "* Titles: ".implode(", ", $arr_episodes)."\n";
+					echo "\n";
+
+				}
+
+			}
+
+
+		}
+
+		echo "[DVD Queue: ".count($queue_episodes)." episodes]\n";
+
 
 	}
