67bbf12397841cf1d62cf54ed6b542ba357eb233
Steve Dibb <steve.dibb@gmail.com>
2014-07-28 21:55:04 -0600

    Add directory functions

--- class.media.episode.php
+++ class.media.episode.php
@@ -20,7 +20,74 @@
 
 		public function __construct($collection_title, $series_title, $episode_title, $export_dir) {
 
-			$this->id = $episode_id;
+			$this->collection_title = $collection_title;
+			$this->series_title = $series_title;
+			$this->episode_title = $episode_title;
+			$this->export_dir = $export_dir;
+
+		}
+
+		public function get_queue_dir() {
+
+			$dir = $this->export_dir;
+			$dir .= "queue/";
+			$dir .= $this->safe_filename_title($this->title)."/";
+
+			return $dir;
+
+		}
+
+		public function create_queue_dir() {
+
+			$dir = $this->get_queue_dir();
+
+			if(!is_dir($dir))
+				return mkdir($dir, 0755, true);
+			else
+				return true;
+
+		}
+
+		public function get_episodes_dir() {
+
+			$dir = $this->export_dir;
+			$dir .= "episodes/";
+			$dir .= $this->safe_filename_title($this->title)."/";
+
+			return $dir;
+
+		}
+
+		public function create_episodes_dir() {
+
+			$dir = $this->get_episodes_dir();
+
+			if(!is_dir($dir))
+				return mkdir($dir, 0755, true);
+			else
+				return true;
+
+		}
+
+		public function get_isos_dir() {
+
+			$dir = $this->export_dir;
+			$dir .= "isos/";
+			$dir .= $this->safe_filename_title($this->collection_title)."/";
+			$dir .= $this->safe_filename_title($this->title)."/";
+
+			return $dir;
+
+		}
+
+		public function create_isos_dir() {
+
+			$dir = $this->get_isos_dir();
+
+			if(!is_dir($dir))
+				return mkdir($dir, 0755, true);
+			else
+				return true;
 
 		}
 
