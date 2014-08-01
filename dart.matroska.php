acd9b2f44e6ef713008363a5a3cb083e42feac47
Steve Dibb <steve.dibb@gmail.com>
2014-08-01 01:03:53 -0600

    Move Matroska section into separate file

---
+++ dart.matroska.php
@@ -0,0 +1,104 @@
+<?php
+
+	/** Matroska Metadata */
+
+	$episode = new MediaEpisode($episode_id, $export_dir);
+	$episodes_model = new Episodes_Model($episode_id);
+	$tracks_model = new Tracks_Model($episode->metadata['track_id']);
+	$series_model = new Series_Model($episode->metadata['series_id']);
+	$dvds_model = new Dvds_Model($episode->metadata['dvd_id']);
+	$queue_model->set_episode_status($episode_id, 3);
+
+	$matroska = new Matroska();
+
+	if($episode->metadata['episode_title'])
+		$matroska->setTitle($episode->metadata['episode_title']);
+
+	$episode_metadata = array(
+		'track_number' => $tracks_model->ix,
+		'starting_chapter' => $episodes_model->starting_chapter,
+		'ending_chapter' => $episodes_model->ending_chapter,
+		'production_studio' => $series_model->production_studio,
+		'production_year' => $series_model->production_year,
+		'season' => $episodes_model->get_season(),
+		'volume' => $episodes_model->get_volume(),
+		'number' => $episodes_model->get_number(),
+		'part' => $episodes_model->part,
+	);
+
+	$matroska->addTag();
+	$matroska->addTarget(70, "COLLECTION");
+	$matroska->addSimpleTag("TITLE", $episode->metadata['series_title']);
+	if($episode_metadata['production_studio'])
+		$matroska->addSimpleTag("PRODUCTION_STUDIO", $episode_metadata['production_studio']);
+	if($episode_metadata['production_year'])
+		$matroska->addSimpleTag("DATE_RELEASE", $episode_metadata['production_year']);
+	$matroska->addSimpleTag("ORIGINAL_MEDIA_TYPE", "DVD");
+
+	// Tag MKV with latest spec I've created
+	$matroska->addSimpleTag("ENCODING_SPEC", "dlna-usb-4");
+
+	// Metadata specification DVD-MKV-1
+	$matroska->addSimpleTag("METADATA_SPEC", "DVD-MKV-1");
+	$matroska->addSimpleTag("DVD_COLLECTION", $episode->metadata['collection_title']);
+	$matroska->addSimpleTag("DVD_SERIES_TITLE", $episode->metadata['series_title']);
+	if($episode_metadata['season'])
+		$matroska->addSimpleTag("DVD_SERIES_SEASON", $episode_metadata['season']);
+	if($episode_metadata['volume'])
+		$matroska->addSimpleTag("DVD_SERIES_VOLUME", $episode_metadata['volume']);
+	$matroska->addSimpleTag("DVD_TRACK_NUMBER", $episode_metadata['track_number']);
+	if($episode_metadata['number'])
+		$matroska->addSimpleTag("DVD_EPISODE_NUMBER", $episode_metadata['number']);
+	$matroska->addSimpleTag("DVD_EPISODE_TITLE", $episode->metadata['episode_title']);
+	if($episode_metadata['part'])
+		$matroska->addSimpleTag("DVD_EPISODE_PART_NUMBER", $episode_metadata['part']);
+	$matroska->addSimpleTag("DVD_ID", $dvd_id);
+	$matroska->addSimpleTag("DVD_SERIES_ID", $series_id);
+	$matroska->addSimpleTag("DVD_TRACK_ID", $track_id);
+	$matroska->addSimpleTag("DVD_EPISODE_ID", $episode_id);
+
+	/** Season **/
+	if($episode_metadata['season']) {
+
+		$matroska->addTag();
+		$matroska->addTarget(60, "SEASON");
+
+		if($episode_metadata['production_year']) {
+			$episode_metadata['year'] = $episode_metadata['production_year'] + $episode_metadata['season'] - 1;
+			$matroska->addSimpleTag("DATE_RELEASE", $episode_metadata['year']);
+		}
+
+		$matroska->addSimpleTag("PART_NUMBER", $episode_metadata['season']);
+
+	}
+
+	/** Episode **/
+	$matroska->addTag();
+	$matroska->addTarget(50, "EPISODE");
+	if($episode->metadata['episode_title'])
+		$matroska->addSimpleTag("TITLE", $episode->metadata['episode_title']);
+	if($episode_metadata['number'])
+		$matroska->addSimpleTag("PART_NUMBER", $episode_metadata['number']);
+	$matroska->addSimpleTag("DATE_TAGGED", date("Y-m-d"));
+	$matroska->addSimpleTag("PLAY_COUNTER", 0);
+
+	if($episode_metadata['part'] > 1) {
+		$matroska->addTag();
+		$matroska->addTarget(40, "PART");
+		$matroska->addSimpleTag("PART_NUMBER", $episode_metadata['part']);
+	}
+
+	if(!file_exists($episode->queue_matroska_mkv) && !file_exists($episode->queue_matroska_xml) && !$dry_run && $handbrake_success) {
+
+		$str = $matroska->getXML();
+
+		if($str) {
+			file_put_contents($episode->queue_matroska_xml, $str);
+			$matroska_xml_success = true;
+		} else {
+			// Creating the XML file failed for some reason
+			$queue_model->set_episode_status($episode_id, 4);
+			$matroska_xml_success = false;
+		}
+
+	}
