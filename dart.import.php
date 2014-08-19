27c0b2fcca696d55b574c922888e39dd01e6beb8
Steve Dibb <steve.dibb@gmail.com>
2014-08-18 18:26:32 -0600

    Add FIXME note to check for null values

--- dart.import.php
+++ dart.import.php
@@ -60,6 +60,7 @@
 			echo "* title: $dvd_title\n";
 			$dvds_model->title = $dvd_title;
 		}
+		// FIXME check for null value
 		if(is_null($dvds_model->longest_track)) {
 			$dvd_longest_track = $dvd->getLongestTrack();
 			echo "* longest track: $dvd_longest_track\n";
