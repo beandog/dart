6ff98d402f20d7543092a5113a8af9a7788a811c
Steve Dibb <steve.dibb@gmail.com>
2014-08-18 21:32:33 -0600

    Update comments

--- dart.import.php
+++ dart.import.php
@@ -83,10 +83,8 @@
 		// If it comes to this point, there's probably an issue reading the DVD
 		// directly.  Either way, the import will still work, so it's debatable
 		// whether this should die here now and kill the progress of the script
-		// or not.  A FIXME is in order in the sense that I don't have error
-		// handling for *extreme* breakage.  This is something where the
-		// dvd_debug program would come into play -- ideally, that would run first
-		// and flag anomalies for me directly.
+		// or not. This is something where the dvd_debug program could come into play.
+		// Ideally, that would run first and flag anomalies for me directly.
 		if(!$dvd_num_tracks) {
 
 			$broken_dvd = true;
