cd587fe69c3cfa18dadf3734b79f9cc483a3cd3a
Steve Dibb <steve.dibb@gmail.com>
2014-08-23 14:07:33 -0600

    Use new DVD class

--- dart.import.subtitles.php
+++ dart.import.subtitles.php
@@ -2,16 +2,13 @@
 
 	/** Subtitles **/
 
-	$subtitle_streams = $dvd_track->getSubtitleStreams();
+	for($subtitle_track = 1; $subtitle_track < $dvd->title_track_subtitle_tracks + 1; $subtitle_track++) {
 
-	foreach($subtitle_streams as $streamid) {
-
-		$dvd_subp = new DVDSubs($xml, $streamid);
+		$dvd->load_subtitle_track($title_track, $subtitle_track);
 
 		// Lookup the database subp.id
 		$subp_model = new Subp_Model;
-		$subp_ix = $dvd_subp->getXMLIX();
-		$subp_model_id = $subp_model->find_subp_id($tracks_model_id, $subp_ix);
+		$subp_model_id = $subp_model->find_subp_id($tracks_model_id, $subtitle_track);
 
 		// Create a new record
 		if(!$subp_model_id) {
@@ -22,7 +19,7 @@
 				echo "! Created new subp id: $subp_model_id\n";
 
 			$subp_model->track_id = $tracks_model_id;
-			$subp_model->ix = $subp_ix;
+			$subp_model->ix = $subtitle_track;
 
 		} else {
 
@@ -30,13 +27,16 @@
 
 		}
 
-		if(!$subp_model->langcode)
-			$subp_model->langcode = $dvd_subp->getLangcode();
+		if($subp_model->langcode != $dvd->subtitle_track_lang_code) {
+			$subp_model->langcode = $dvd->subtitle_track_lang_code;
+			if($debug)
+				echo "* Updating subtitle lang code: ".$dvd->subtitle_track_lang_code."\n";
+		}
 
-		if(!$subp_model->language)
-			$subp_model->language = $dvd_subp->getLanguage();
-
-		if(!$subp_model->streamid)
-			$subp_model->streamid = $streamid;
+		if(!$subp_model->streamid) {
+			$subp_model->streamid = $dvd->subtitle_track_stream_id;
+			if($debug)
+				echo "* Updating subtitle stream id: ".$dvd->subtitle_track_stream_id."\n";
+		}
 
 	}
