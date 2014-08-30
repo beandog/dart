fe5a41f5c701b7a79236c2d5988928c50beef18e
Steve Dibb <steve.dibb@gmail.com>
2014-08-30 16:12:34 -0600

    Filter out channels that are not marked as active

--- models/tracks.php
+++ models/tracks.php
@@ -29,7 +29,7 @@
 
 		public function get_audio_streams() {
 
-			$sql = "SELECT * FROM audio WHERE track_id = ".$this->db->quote($this->id)." ORDER BY langcode = 'en' DESC, channels DESC, format = 'dts' DESC, streamid;";
+			$sql = "SELECT * FROM audio WHERE track_id = ".$this->db->quote($this->id)." AND active = 1 ORDER BY langcode = 'en' DESC, channels DESC, format = 'dts' DESC, streamid;";
 
 			$arr = $this->db->getAll($sql);
 
@@ -39,9 +39,7 @@
 
 		public function get_best_quality_audio_streamid() {
 
-			$sql = "SELECT COALESCE(streamid, '0x80') FROM audio WHERE track_id = ".$this->db->quote($this->id)." AND channels = (SELECT MAX(channels) FROM audio WHERE track_id = ".$this->db->quote($this->id).") ORDER BY CASE WHEN format = 'dts' THEN 1 ELSE 0 END, streamid LIMIT 1;";
-
-			$sql = "SELECT COALESCE(streamid, '0x80') FROM audio WHERE track_id = ".$this->db->quote($this->id)." AND langcode = 'en' AND channels = (SELECT MAX(channels) FROM audio WHERE track_id = ".$this->db->quote($this->id).") ORDER BY CASE WHEN format = 'dts' THEN 0 ELSE 1 END, streamid LIMIT 1;";
+			$sql = "SELECT COALESCE(streamid, '0x80') FROM audio WHERE track_id = ".$this->db->quote($this->id)." AND langcode = 'en' AND active = 1 AND channels = (SELECT MAX(channels) FROM audio WHERE track_id = ".$this->db->quote($this->id).") ORDER BY CASE WHEN format = 'dts' THEN 0 ELSE 1 END, streamid LIMIT 1;";
 
 			$var = $this->db->getOne($sql);
 
@@ -51,7 +49,7 @@
 
 		public function get_first_english_streamid() {
 
-			$sql = "SELECT COALESCE(streamid, '0x80') FROM audio WHERE track_id = ".$this->db->quote($this->id)." AND langcode = 'en' ORDER BY streamid LIMIT 1;";
+			$sql = "SELECT COALESCE(streamid, '0x80') FROM audio WHERE track_id = ".$this->db->quote($this->id)." AND langcode = 'en' AND active = 1 ORDER BY streamid LIMIT 1;";
 
 			$var = $this->db->getOne($sql);
 
@@ -61,7 +59,7 @@
 
 		public function get_first_english_subp() {
 
-			$sql = "SELECT ix FROM subp WHERE track_id = ".$this->db->quote($this->id)." AND langcode = 'en' ORDER BY ix LIMIT 1;";
+			$sql = "SELECT ix FROM subp WHERE track_id = ".$this->db->quote($this->id)." AND langcode = 'en' AND active = 1 ORDER BY ix LIMIT 1;";
 
 			$var = $this->db->getOne($sql);
 
