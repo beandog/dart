b869676ab71ae60d8e4767c6a4e3086b26199c8e
Steve Dibb <steve.dibb@gmail.com>
2014-10-05 18:03:12 -0600

    Move safe_filename_title function to a global one

--- dart.functions.php
+++ dart.functions.php
@@ -68,4 +68,14 @@
 
 	}
 
+	function safe_filename_title($str = 'Title', $underlines = false) {
+
+		$str = preg_replace("/[^A-Za-z0-9 _]/", '', $str);
+		$str = str_replace("/", "-", $str);
+		if($underlines)
+			$str = str_replace(' ', '_', $str);
+		return $str;
+
+	}
+
 ?>
