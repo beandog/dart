81e4ed5b5777c8f508bbc76933dfb45e1e20811a
Steve Dibb <steve.dibb@gmail.com>
2014-10-15 14:49:06 -0600

    New files to handle final stages

---
+++ dart.remove.stage.php
@@ -0,0 +1,87 @@
+<?php
+
+	$remove_queue = false;
+	$remove_series = false;
+	$remove_stage_passed = false;
+	$remove_stage_skipped = false;
+	$remove_stage_complete = false;
+
+	// Skip the stage if the episode queue directory is already gone
+	if(!is_dir($episode_queue_dir))
+		$remove_stage_skipped = true;
+
+	// Skip the stage if the series queue dir is already gone
+	if(!is_dir($series_queue_dir))
+		$remove_stage_skipped = true;
+
+	// If the target filename exists, removing the queue is okay
+	if(file_exists($target_files['episode_mkv']))
+		$remove_queue = true;
+
+	// If debugging, don't remove the queue
+	if($debug) {
+		$remove_queue = false;
+		$remove_stage_skipped = true;
+	}
+
+	// Override all settings if renaming is forced
+	if($force_final) {
+		$remove_queue = true;
+		$remove_stage_skipped = false;
+	}
+
+	// Ignore everything on a dry run
+	if($dry_run) {
+		$remove_queue = false;
+		$remove_stage_skipped = true;
+	}
+
+	// Skip the remove stage
+	if($remove_stage_skipped)
+		goto remove_stage_complete;
+
+	// Only remove the files that were specifically created by the encode
+	// stages.  If extra files are there, ignore them.
+	if($remove_queue) {
+
+		if(file_exists($queue_files['handbrake_script']))
+			unlink($queue_files['handbrake_script']);
+		if(file_exists($queue_files['handbrake_log']))
+			unlink($queue_files['handbrake_log']);
+		if(file_exists($queue_files['handbrake_output_filename']))
+			unlink($queue_files['handbrake_output_filename']);
+		if(file_exists($queue_files['metadata_xml_file']))
+			unlink($queue_files['metadata_xml_file']);
+		if(file_exists($queue_files['mkvmerge_script']))
+			unlink($queue_files['mkvmerge_script']);
+		if(file_exists($queue_files['mkvmerge_log']))
+			unlink($queue_files['mkvmerge_log']);
+		if(file_exists($queue_files['mkvmerge_output_filename']))
+			unlink($queue_files['mkvmerge_output_filename']);
+
+		// Remove the episode queue dir
+		$scandir = scandir($episode_queue_dir);
+		if(count($scandir) == 2)
+			unlink($episode_queue_dir);
+
+		// Check to see if there are any more than the ISO symlink left
+		// in the series queue dir
+		$scandir = scandir($series_queue_dir);
+		if(count($scandir) <= 3)
+			$remove_series = true;
+
+	}
+
+	// Remove the series queue dir
+	if($remove_series) {
+
+		if(is_link($queue_files['dvd_iso_symlink']))
+			unlink($queue_files['dvd_iso_symlink']);
+
+		unlink($series_queue_dir);
+
+	}
+
+	remove_stage_complete:
+
+	$remove_stage_complete = true;
---
+++ dart.rename.stage.php
@@ -0,0 +1,40 @@
+<?php
+
+	$rename_video = false;
+	$rename_stage_passed = false;
+	$rename_stage_skipped = false;
+	$rename_stage_complete = false;
+
+	// If the final target file exists, skip the encoding stage
+	if(file_exists($target_files['episode_mkv']))
+		$rename_stage_skipped = true;
+
+	// Rename the video if the target file does not exist, and the queue
+	// source file does.
+	if(!file_exists($target_files['episode_mkv']))
+		if(file_exists($queue_files['mkvmerge_output_filename']))
+			$rename_video = true;
+
+	// Override all settings if renaming is forced
+	if($force_final) {
+		$rename_video = true;
+		$rename_stage_skipped = false;
+	}
+
+	// Ignore everything on a dry run
+	if($dry_run) {
+		$rename_video = false;
+		$rename_stage_skipped = false;
+	}
+
+	// Skip the rename
+	if($rename_stage_skipped)
+		goto rename_stage_complete;
+
+	// Rename the video file
+	if($rename_video)
+		$rename_stage_passed = rename($queue_files['mkvmerge_output_filename'], $target_files['episode_mkv'])
+
+	rename_stage_complete:
+
+	$rename_stage_complete = true;
