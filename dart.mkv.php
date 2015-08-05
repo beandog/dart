03261f081ee4e142ab090e3ccf460df1eef56e5b
Steve Dibb <steve.dibb@gmail.com>
2015-08-04 19:48:57 -0600

    Update to dvd-mkv-4 spec; add an episode_title tag directly to make parsing info simpler; always add episode part number if it is 1 or higher; remove ambiguous episode_number which can be changed easily in the DVDs admin

--- dart.mkv.php
+++ dart.mkv.php
@@ -7,14 +7,23 @@
 
 	$matroska->setTitle($episode_title);
 
+	// Due to difficulty pulling out nested information, this adds both
+	// simple tags for quick parsing, and the structured metadata tags
+	// as well.
+
 	$matroska->addTag();
-	$matroska->addSimpleTag("encoding_spec", "dlna-usb-5");
-	$matroska->addSimpleTag("metadata_spec", "dvd-mkv-3");
+	if($encoding_spec)
+		$matroska->addSimpleTag("encoding_spec", $encoding_spec);
+	if($metadata_spec)
+		$matroska->addSimpleTag("metadata_spec", $metadata_spec);
 	$matroska->addSimpleTag("handbrake_version", $handbrake_version);
 	$matroska->addSimpleTag("collection_title", $collection_title);
 	$matroska->addSimpleTag("series_title", $series_title);
 	$matroska->addSimpleTag("dvd_id", $episode['dvd_id']);
 	$matroska->addSimpleTag("episode_id", $episode_id);
+	$matroska->addSimpleTag("episode_title", $episode_title);
+	if($episode['part'])
+		$matroska->addSimpleTag("episode_part", $episode['part']);
 
 	// See http://matroska.org/files/tags/simpsons-s01e01.xml for an example of a DVD
 	// from a TV show series.
@@ -28,12 +37,10 @@
 	$matroska->addTag();
 	$matroska->addTarget(50, "EPISODE");
 	$matroska->addSimpleTag("TITLE", $episode_title);
-	if($episode_number)
-		$matroska->addSimpleTag("PART_NUMBER", $episode_number);
 	$matroska->addSimpleTag("DATE_TAGGED", date("Y-m-d"));
 	$matroska->addSimpleTag("PLAY_COUNTER", 0);
 
-	if($episode['part'] > 1) {
+	if($episode['part']) {
 		$matroska->addTag();
 		$matroska->addTarget(40, "PART");
 		$matroska->addSimpleTag("PART_NUMBER", $episode['part']);
