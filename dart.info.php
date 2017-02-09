908ec2a3005d0178bed6fe34a481e4880316ab78
Steve Dibb <steve.dibb@gmail.com>
2017-02-08 23:10:14 -0700

    Remove extra output

--- dart.info.php
+++ dart.info.php
@@ -1,19 +1,5 @@
 <?php
 
-	// Display encode instructions about a disc
-	if($opt_encode_info) {
-
-		$dvd_episodes = $dvds_model->get_episodes();
-
-		// Display the episode names
-		foreach($dvd_episodes as $episode_id) {
-			$episodes_model = new Episodes_Model($episode_id);
-			$display_name = $episodes_model->get_display_name();
-			echo "* $display_name\n";
-		}
-
-	}
-
 	// Display info about disc
 	if($opt_info) {
 
