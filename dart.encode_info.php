7bff89e9333d1cfbd34c536c0fd7b6d7bb6b9692
Steve Dibb <steve.dibb@gmail.com>
2017-02-12 16:36:17 -0700

    Remove variables that can be pulled from model

--- dart.encode_info.php
+++ dart.encode_info.php
@@ -12,13 +12,11 @@
 			$tracks_model = new Tracks_Model($episodes_model->track_id);
 			$series_model = new Series_Model($episodes_model->get_series_id());
 
-			$episode_metadata = $episodes_model->get_metadata();
-
 			$filename = str_pad($dvds_model->get_collection_id(), 1, '0');
 			$filename .= ".".str_pad($dvds_model->get_series_id(), 3, '0', STR_PAD_LEFT);
 			$filename .= ".".str_pad($dvds_model->id, 4, '0', STR_PAD_LEFT);
 			$filename .= ".".str_pad($episode_id, 5, 0, STR_PAD_LEFT);
-			$filename .= ".".$episode_metadata['nsix'];
+			$filename .= ".".$series_model->nsix;
 			$filename .= ".$container";
 
 			if(!($opt_skip_existing && file_exists($filename))) {
