764891671255914b23dcb4898068cd1ff53ca7b5
Steve Dibb <steve.dibb@gmail.com>
2017-03-01 11:45:49 -0700

    New simple script to convert mkv to mp4 with avconv

---
+++ convert_mkv_to_mp4.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+
+	if($argc == 1)
+		exit(1);
+	
+	$arr_files = array_slice($argv, 1);
+
+	foreach($arr_files as $mkv) {
+
+		$mkv = realpath($mkv);
+
+		if(!file_exists($mkv))
+			continue;
+
+		$pathinfo = pathinfo($mkv);
+
+		if($pathinfo['extension'] != 'mkv' || $pathinfo['extension'] == 'mp4')
+			continue;
+
+		$mp4 = $pathinfo['dirname'].'/'.$pathinfo['filename'].'.mp4';
+
+		if(file_exists($mp4))
+			continue;
+
+		$cmd = "avconv -y -i ".escapeshellarg($mkv)." -vcodec copy -acodec copy -scodec copy ".escapeshellarg($mp4);
+
+		exec($cmd, $output, $retval);
+	
+	}
