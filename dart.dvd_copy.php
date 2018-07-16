a7b85bc18783e3f4ea439ab22edded2415693011
Steve Dibb <steve.dibb@gmail.com>
2018-07-16 15:45:35 -0600

    Add dvd_copy

---
+++ dart.dvd_copy.php
@@ -0,0 +1,21 @@
+<?php
+
+if($opt_copy_info && $episode_id) {
+
+	$dvd_copy = new DVDCopy();
+	$dvd_copy->set_binary('dvd_copy');
+
+	if($debug)
+		$dvd_copy->debug();
+
+	if($verbose)
+		$dvd_copy->verbose();
+
+	if($dry_run)
+		$dvd_copy->dry_run();
+
+	$dvd_copy->input_filename($device);
+	$dvd_copy->input_track($tracks_model->ix);
+	$dvd_copy->set_chapters($episodes_model->starting_chapter, $episodes_model->ending_chapter);
+
+}
