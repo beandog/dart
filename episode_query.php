a9dfe8134d775a99b9916317d9adaa43ea26edb8
Steve Dibb <steve.dibb@gmail.com>
2019-08-10 00:30:17 -0600

    Import changes from dlna

--- episode_query.php
+++ episode_query.php
@@ -78,7 +78,7 @@
 	$realpath = realpath($filename);
 	$pathinfo = pathinfo($realpath);
 	$movie = false;
-	if(substr($pathinfo['basename'], 0, 1) == '4' || substr($pathinfo['basename'], 0, 1) == '8')
+	if(substr($pathinfo['basename'], 0, 1) == '3' || substr($pathinfo['basename'], 0, 1) == '4' || substr($pathinfo['basename'], 0, 1) == '5' || substr($pathinfo['basename'], 0, 1) == '8')
 		$movie = true;
 	$str_elements = explode('.', $pathinfo['basename']);
 	if(count($str_elements) < 3 || !file_exists($realpath) || !(in_array($pathinfo['extension'], array('mkv', 'mp4', 'mpg', 'vob', 'm2ts')))) {
@@ -99,6 +99,7 @@
 
 	$episode_metadata = $episodes_model->get_metadata();
 
+	// If season is set as '100' in database, it's flagged as as 'Special' for Plex to pick up
 	if($episode_metadata['season'] == 100)
 		$episode_metadata['season'] = 0;
 
