969706390273dada922dd4caeddc398e2ad67522
Steve Dibb <steve.dibb@gmail.com>
2021-01-24 20:27:16 -0700

    Fix spacing

--- class.bluray.php
+++ class.bluray.php
@@ -110,7 +110,7 @@
 			$arg_device = escapeshellarg($this->device);
 			$cmd = $this->binary." --json $arg_device";
 			if(!$this->debug)
-				$cmd .= "2> /dev/null";
+				$cmd .= " 2> /dev/null";
 
 			if($this->debug)
 				echo "* Executing: $cmd\n";
