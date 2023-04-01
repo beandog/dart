dfd9098d6b6ab0a7b08bf3f13acd1a50e657f91c
Steve Dibb <steve.dibb@gmail.com>
2023-04-01 10:41:12 -0600

    Remove MP3 as an audio codec option

--- dart.vp8.php
+++ dart.vp8.php
@@ -82,7 +82,7 @@ if($opt_encode_info && $episode_id && $video_encoder == 'vp8') {
 		$handbrake->add_audio_track($tracks_model->audio_ix);
 
 	$audio_encoder = $series_model->get_audio_encoder();
-	if($audio_encoder == 'fdk_aac' || $audio_encoder == 'mp3' || $audio_encoder == 'ac3' || $audio_encoder == 'eac3') {
+	if($audio_encoder == 'fdk_aac' || $audio_encoder == 'ac3' || $audio_encoder == 'eac3') {
 		$handbrake->add_audio_encoder($audio_encoder);
 	} elseif($audio_encoder == 'fdk_aac,copy') {
 		$handbrake->add_audio_encoder('fdk_aac');
