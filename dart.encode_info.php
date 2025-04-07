b5d6048b689ef3ec90214db88296cc15721d7d65
Steve Dibb <steve.dibb@gmail.com>
2025-04-06 19:54:55 -0600

    Always set video format

--- dart.encode_info.php
+++ dart.encode_info.php
@@ -175,6 +175,8 @@ if($disc_indexed && ($opt_encode_info || $opt_copy_info || $opt_ffplay || $opt_f
 
 			$handbrake->input_filename($input_filename);
 
+			$handbrake->set_video_format($tracks_model->format);
+
 			$handbrake->output_filename($filename);
 
 			if($opt_qa) {
