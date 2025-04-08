236af237875c65cd3f8e05f26032b0d17e9c0bbe
Steve Dibb <steve.dibb@gmail.com>
2025-04-07 22:33:51 -0600

    Fix using --ffpipe

--- dart.encode_info.php
+++ dart.encode_info.php
@@ -16,6 +16,8 @@ if($disc_indexed && ($opt_encode_info || $opt_copy_info || $opt_ffplay || $opt_f
 		$dvd_encoder = 'ffmpeg';
 	elseif($opt_ffprobe)
 		$dvd_encoder = 'ffprobe';
+	elseif($opt_ffpipe)
+		$dvd_encoder = 'ffpipe';
 
 	$dvd_episodes = $dvds_model->get_episodes();
 
