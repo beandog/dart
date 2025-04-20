04f01c033a66560621af9869c20a15b5bd82f5e0
Steve Dibb <steve.dibb@gmail.com>
2025-04-20 14:14:57 -0600

    Remove using old x264 include file

--- dart.encode_info.php
+++ dart.encode_info.php
@@ -209,9 +209,77 @@ if($disc_indexed && ($opt_encode_info || $opt_copy_info || $opt_ffplay || $opt_f
 		 */
 		if(!($opt_skip_existing && file_exists($filename)) && $disc_type == "dvd" && $opt_encode_info && $dvd_encoder == 'handbrake') {
 
-			require 'dart.x264.php';
+			$handbrake = new HandBrake;
+			$handbrake->set_binary($handbrake_bin);
+			$handbrake->verbose($verbose);
+			$handbrake->debug($debug);
 
-			$handbrake->input_filename($input_filename);
+			/** Files **/
+
+			$handbrake->input_filename($device);
+			$handbrake->input_track($tracks_model->ix);
+
+			/** Video **/
+
+			$handbrake->set_vcodec($vcodec);
+
+			$video_quality = $series_model->get_crf();
+			if($arg_crf)
+				$video_quality = abs(intval($arg_crf));
+			$handbrake->set_video_quality($video_quality);
+
+			if($opt_fast)
+				$handbrake->set_x264_preset('ultrafast');
+			elseif($opt_slow)
+				$handbrake->set_x264_preset('slow');
+
+			$x264_tune = $series_model->get_x264_tune();
+			if($vcodec == 'x264' && $x264_tune)
+				$handbrake->set_x264_tune($x264_tune);
+
+			/** Frame and fields **/
+
+			// Set framerate
+			if($fps)
+				$handbrake->set_video_framerate($fps);
+
+			/** Audio **/
+
+			$handbrake->add_audio_track($tracks_model->audio_ix);
+
+			$acodec = $series_model->get_acodec();
+			if($acodec == 'fdk_aac' || $acodec == 'ac3' || $acodec == 'flac' || $acodec == 'mp3') {
+				$handbrake->add_acodec($acodec);
+				if($acodec == 'mp3')
+					$handbrake->set_audio_bitrate('320k');
+			} elseif($acodec == 'fdk_aac,copy') {
+				$handbrake->add_acodec('fdk_aac');
+				$handbrake->add_acodec('copy');
+			} else {
+				$handbrake->add_acodec('copy');
+			}
+
+			/** Subtitles **/
+
+			// Check for a subtitle track
+
+			$subp_ix = $tracks_model->get_first_english_subp();
+			$has_closed_captioning = $tracks_model->has_closed_captioning();
+
+			// If we have a VobSub one, add it
+			// Otherwise, check for a CC stream, and add that
+			if($subp_ix) {
+				$handbrake->add_subtitle_track($subp_ix);
+			} elseif($has_closed_captioning) {
+				$num_subp_tracks = $tracks_model->get_num_active_subp_tracks();
+				$closed_captioning_ix = $num_subp_tracks + 1;
+				$handbrake->add_subtitle_track($closed_captioning_ix);
+			}
+
+			/** Chapters **/
+
+			$handbrake->set_chapters($episodes_model->starting_chapter, $episodes_model->ending_chapter);
+			$handbrake->add_chapters();
 
 			$handbrake->set_video_format($tracks_model->format);
 
