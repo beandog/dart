a5dc218ebcd1ffff4d3fd67d00c777a428c97df0
Steve Dibb <steve.dibb@gmail.com>
2025-04-26 00:57:49 -0600

    Remove old function

--- dart.functions.php
+++ dart.functions.php
@@ -39,17 +39,6 @@
 
 	}
 
-	// Switch to the next device
-	function toggle_device($all, $current) {
-
-		$current_key = array_search($current, $all);
-		if(array_key_exists($current_key + 1, $all))
-			return $all[$current_key + 1];
-		else
-			return $all[0];
-
-	}
-
 	function beep_error() {
 
 		system("beep -f 1000 -n -f 2000 -n -f 1500 -n -f 1750 -n f 1750 -n -f 1750");
