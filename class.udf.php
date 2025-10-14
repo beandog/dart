9bed1d016d89ba120de5b90ab99023331b615ea1
Steve Dibb <steve.dibb@gmail.com>
2025-10-13 20:54:27 -0600

    Add UDF class to get info of disc using 'udfinfo' from 'udftools' package

---
+++ class.udf.php
@@ -0,0 +1,53 @@
+<?php
+
+	class UDF {
+
+		// UDF Info
+		public $udf_info;
+		public $udf_uuid;
+		public $udf_blocksize;
+		public $udf_blocks;
+		public $udf_numfiles;
+		public $udf_numdirs;
+		public $udf_udfrev;
+
+		function __construct($device = "/dev/bluray", $debug = false) {
+
+			// Get udfinfo
+			$arg_device = escapeshellarg($device);
+			$cmd = "udfinfo $arg_device";
+
+			if(!$debug)
+				$cmd .= " 2> /dev/null";
+
+			if($debug)
+				echo "* Executing: $cmd\n";
+
+			exec($cmd, $output, $retval);
+
+			if($retval !== 0 || !count($output)) {
+				echo "* udfinfo FAILED\n";
+				return false;
+			}
+
+			foreach($output as $str) {
+				$arr = explode('=', $str);
+				$arr_udfinfo[$arr[0]] = trim($arr[1]);
+			}
+
+			extract($arr_udfinfo);
+
+			$this->udf_info = array(
+				'uuid' => $uuid,
+				'blocksize' => $blocksize,
+				'blocks' => $blocks,
+				'numfiles' => $numfiles,
+				'numdirs' => $numdirs,
+				'udfrev' => $udfrev,
+			);
+
+			return true;
+
+		}
+
+	}
