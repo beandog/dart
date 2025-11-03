aea2ae0d23d2b562bb5852e98abe6ac3faea5301
Steve Dibb <steve.dibb@gmail.com>
2025-11-03 16:05:53 -0700

    Use MP4 by default

--- dart.encode_info.php
+++ dart.encode_info.php
@@ -45,7 +45,7 @@ if($disc_indexed && ($opt_encode_info || $opt_copy_info || $opt_ffplay || $opt_f
 
 	$input_filename = realpath($device);
 
-	$container = 'mkv';
+	$container = 'mp4';
 
 	if($opt_mp4 || (substr($arg_vcodec, 0, 3) == 'nvenc'))
 		$container = 'mp4';
