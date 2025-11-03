71cf3705fc7e0a3fca86002c5c14c7c98fc8a186
Steve Dibb <steve.dibb@gmail.com>
2025-11-02 22:25:42 -0700

    Add option to remux

--- reencode_mkv.php
+++ reencode_mkv.php
@@ -26,6 +26,12 @@
 		'action' => 'StoreTrue',
 		'default' => false,
 	));
+	$parser->addOption('opt_remux', array(
+		'long_name' => '--remux',
+		'description' => 'Encode audio track and remux with mkvmerge',
+		'action' => 'StoreTrue',
+		'default' => false,
+	));
 
 	try { $result = $parser->parse(); }
 	catch(PEAR_Exception $e) {
@@ -37,7 +43,12 @@
 	extract($result->options);
 
 	if(!$opt_mkv && !$opt_mp4)
-		$opt_mp4 = true;
+		$opt_mkv = true;
+
+	if($opt_remux) {
+		$opt_mkv = false;
+		$opt_mp4 = false;
+	}
 
 	foreach($filenames as $filename) {
 
@@ -50,14 +61,16 @@
 		$basename = basename($filename, '.mkv');
 		if($opt_mp4)
 			$video = "v2-$basename.mp4";
-		else
+		elseif($opt_mkv || $opt_remux)
 			$video = "v2-$basename.mkv";
+		$audio = "v2-$basename.aac";
 
 		if(file_exists($video))
 			continue;
 
 		$arg_input_filename = escapeshellarg($filename);
 		$arg_output_filename = escapeshellarg($video);
+		$arg_aac_filename = escapeshellarg($audio);
 
 		// ffmpeg arguments:
 		// - re-encode audio to AAC with same channels as source and highest VBR settings
@@ -71,10 +84,21 @@
 		if($opt_mkv)
 			$ffmpeg_opts = "-i $arg_input_filename -vcodec 'copy' -acodec 'libfdk_aac' -vbr '5' -sn -map_metadata '0' -y $arg_output_filename";
 
+		if($opt_remux) {
+			$ffmpeg_opts = "-i $arg_input_filename -vn -acodec 'libfdk_aac' -vbr '5' -sn -map_chapters '-1' -map_metadata '-1' -metadata:s 'language=eng' -y $arg_aac_filename";
+		}
 		if($opt_batch)
 			$ffmpeg_opts = "-v quiet -stats $ffmpeg_opts";
 
-		echo "ffmpeg $ffmpeg_opts\n";
+		if(!$opt_remux || ($opt_remux && !file_exists($audio)))
+			echo "ffmpeg $ffmpeg_opts\n";
+
+		if($opt_remux) {
+
+			$mkvmerge_cmd = "mkvmerge -o $arg_output_filename -A -S $arg_input_filename $arg_aac_filename";
+			echo "$mkvmerge_cmd\n";
+
+		}
 
 	}
 
