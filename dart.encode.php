a530b6a56d2d4cbf04337517c10aa7cba766ffbb
Steve Dibb <steve.dibb@gmail.com>
2026-03-26 12:48:00 -0600

    Support ffplay when source needs to be copied from stream

--- dart.encode.php
+++ dart.encode.php
@@ -197,7 +197,59 @@ if($disc_indexed && ($opt_encode_info || $opt_ffplay || $opt_ffprobe || $opt_sca
 		require 'dart.encode_handbrake.php';
 		require 'dart.encode_ffmpeg.php';
 
-		if($disc_type == 'dvd' && $opt_ffplay) {
+		if($disc_type == 'dvd' && $opt_ffplay && $dvd_encoder == 'ffpipe') {
+
+			$ffmpeg = new FFMpeg();
+			$ffmpeg->set_encoder('ffpipe');
+
+			$ffmpeg->input_filename('-');
+
+			$dvd_copy = new DVDCopy();
+
+			$dvd_copy->input_filename($input_filename);
+			$dvd_copy->output_filename('-');
+			$dvd_copy->input_track($tracks_model->ix);
+			$dvd_copy->set_chapters($episodes_model->starting_chapter, $episodes_model->ending_chapter);
+
+			$dvd_copy_command = $dvd_copy->get_executable_string();
+
+			$dvd_copy_command .= ' 2> /dev/null';
+
+			if($debug)
+				$ffmpeg->debug();
+
+			if($verbose)
+				$ffmpeg->verbose();
+
+			/** Video **/
+			$deint_filter = "bwdif=deint=$video_deint";
+			$ffmpeg->add_video_filter($deint_filter);
+
+			if($fps)
+				$ffmpeg->add_video_filter("fps=$fps");
+
+			if($arg_vf)
+				$ffmpeg->add_video_filter($arg_vf);
+
+			if($opt_qa)
+				$ffmpeg->set_duration($qa_max);
+
+			$ffmpeg->fullscreen();
+
+			$ffmpeg_command = $ffmpeg->get_executable_string();
+
+			// Note that ffplay will display VOBSUB subtitles by default, which is nice to see if they have them
+			if($opt_no_subtitles)
+				$ffmpeg_command .= ' -sn';
+
+			$ffplay_command = "$dvd_copy_command | $ffmpeg_command";
+
+			if($opt_encode_info)
+				echo "$ffplay_command\n";
+
+		}
+
+		if($disc_type == 'dvd' && $opt_ffplay && $dvd_encoder != 'ffpipe') {
 
 			$ffmpeg = new FFMpeg();
 			$ffmpeg->set_encoder('ffplay');
