b5d8f370b8eaca3ec312c3ec08fac650a8690b1e
Steve Dibb <steve.dibb@gmail.com>
2026-04-24 14:45:08 -0600

    Break out function to get id from dvdread_id into one loading as well

--- models/pdo.series_dvds.php
+++ models/pdo.series_dvds.php
@@ -10,7 +10,7 @@
 
 		}
 
-		function get_dvdread_id_iso_filename($dvdread_id) {
+		function load_dvdread_id($dvdread_id) {
 
 			$dvdread_id = trim(strval($dvdread_id));
 			if($dvdread_id === '') {
@@ -20,7 +20,19 @@
 
 			$dvdread_id = $this->quote($dvdread_id);
 
-			$sql = "SELECT iso_filename FROM view_series_dvds WHERE dvdread_id = $dvdread_id;";
+			$sql = "SELECT id FROM view_series_dvds WHERE dvdread_id = $dvdread_id;";
+
+			$this->id = $this->get_one($sql);
+
+			return $this->id;
+
+		}
+
+		function get_dvdread_id_iso_filename($dvdread_id) {
+
+			$this->load_dvdread_id($dvdread_id);
+
+			$sql = "SELECT iso_filename FROM view_series_dvds WHERE id = ".$this->id.";";
 
 			$var = $this->get_one($sql);
 
