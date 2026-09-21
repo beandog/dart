a853ec17b308c9d61f21373d3d2155c693a3cbd8
Steve Dibb <steve.dibb@gmail.com>
2026-09-21 02:04:26 -0600

    Disable features not available on GTX 1060

--- dart.encode_ffmpeg.php
+++ dart.encode_ffmpeg.php
@@ -65,10 +65,12 @@ if($disc_type == 'dvd' && $dvd_encoder == 'ffmpeg') {
 	$ffmpeg->add_argument('maxrate:v', '0');
 	$ffmpeg->add_argument('rc-lookahead', '32');
 	$ffmpeg->add_argument('spatial_aq', '1');
-	$ffmpeg->add_argument('temporal_aq', '1');
 	$ffmpeg->add_argument('aq-strength', '10');
-	$ffmpeg->add_argument('bf', '3');
-	$ffmpeg->add_argument('b_ref_mode', 'middle');
+	if($hostname != 'tobe') {
+		$ffmpeg->add_argument('temporal_aq', '1');
+		$ffmpeg->add_argument('bf', '3');
+		$ffmpeg->add_argument('b_ref_mode', 'middle');
+	}
 	$ffmpeg->add_argument('multipass', 'fullres');
 	$ffmpeg->add_argument('map', 'v');
 
