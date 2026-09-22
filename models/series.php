7800cf4128b22f1fac26a8de0a208e14bdeb88cb
Steve Dibb <steve.dibb@gmail.com>
2026-09-21 20:51:11 -0600

    Remove getting fps from preset

--- models/series.php
+++ models/series.php
@@ -100,16 +100,6 @@
 
 		}
 
-		function get_preset_fps() {
-
-			$sql = "SELECT presets.fps FROM presets INNER JOIN series_presets ON series_presets.preset_id = presets.id AND series_presets.series_id = ".$this->id.";";
-
-			$var = $this->get_one($sql);
-
-			return $var;
-
-		}
-
 		function get_library_name() {
 
 			$sql = "SELECT l.name FROM series s INNER JOIN libraries l ON s.library_id = l.id WHERE s.id = ".$this->id.";";
